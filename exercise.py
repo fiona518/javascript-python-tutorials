ef3dfb33a4372e0c571f9260e5b90f7ddfb20649
fiona518 <whe036@aucklanduni.ac.nz>
2022-09-16 14:50:33 +1200

    Tkinker place()

--- exercise.py
+++ exercise.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from tkinter import *
 
-parent = Tk()
-name = Label(parent, text="Name").grid(row=0, column=0)
-e1 = Entry(parent).grid(row=0, column=1)
-password = Label(parent, text="Password").grid(row=1, column=0)
-e2 = Entry(parent).grid(row=1, column=1)
-submit = Button(parent, text="Submit").grid(row=4, column=0)
-parent.mainloop()
+top = Tk()
+top.geometry('400x250')
+name = Label(top, text="Name").place(x=30, y=50)
+email = Label(top, text="Email").place(x=30, y=90)
+password = Label(top, text="Password").place(x=30, y=130)
+e1 = Entry(top).place(x=80, y=50)
+e2 = Entry(top).place(x=80, y=90)
+e3 = Entry(top).place(x=80, y=130)
+top.mainloop()
