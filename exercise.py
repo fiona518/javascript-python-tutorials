d143d23ca6e5fc903d030b7dca13e236ab176ba4
fiona518 <whe036@aucklanduni.ac.nz>
2022-09-16 14:42:56 +1200

    Tkinter pack()

--- exercise.py
+++ exercise.py
@@ -1,16 +1,13 @@
-import mysql.connector
+#!/usr/bin/python3
+from tkinter import *
 
-myconn = mysql.connector.connect(host = "localhost", user = "root", passwd = "G5g8oqp4!", database="pythonDB2")
-  
-#creating the cursor object  
-cur = myconn.cursor()
-
-try: 
-    result = cur.execute("select Employee.id, Employee.name, Employee.salary, Departments.Dept_id, Departments.Dept_Name from Departments left join Employee on Departments.Dept_id = Employee.Dept_id")
-    print("ID    Name    Salary    Dept_Id    Dept_Name")  
-    for row in cur:  
-        print(row[0],"    ", row[1],"    ",row[2],"    ",row[3],"    ",row[4])  
-except:
-    myconn.rollback()
-  
-myconn.close()
+parent = Tk()
+redbutton = Button(parent, text="Red", fg="red")
+redbutton.pack(side=LEFT)
+greenbutton = Button(parent, text="Black", fg="black")
+greenbutton.pack(side=RIGHT)
+bluebutton = Button(parent, text="Blue", fg="blue")
+bluebutton.pack(side=TOP)
+blackbutton = Button(parent, text="Green", fg="red")
+blackbutton.pack(side=BOTTOM)
+parent.mainloop()
