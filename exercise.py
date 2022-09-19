627c7513208495fd7b7b32e87fefb1a9ac1dc199
fiona518 <whe036@aucklanduni.ac.nz>
2022-09-19 13:03:40 +1200

    tkinter radio

--- exercise.py
+++ exercise.py
@@ -1,9 +1,25 @@
+from cProfile import label
 from tkinter import *
 
-top =  Tk()
-top.geometry("100x100")
-var = StringVar()
-msg = Message(top, text="Welcome to Javatpoint")
+def selection():
+    selection = "You selected the option " + str(radio.get())
+    label.config(text = selection)
 
-msg.pack()
+top =  Tk()
+top.geometry("300x150")
+radio = IntVar()
+lbl = Label(text="Favourite programming language.")
+lbl.pack()
+
+R1 = Radiobutton(top, text="C", variable=radio, value=1, command=selection)
+R1.pack(anchor=W)
+
+R2 = Radiobutton(top, text="C++", variable=radio, value=2, command=selection)
+R2.pack(anchor=W)
+
+R3 = Radiobutton(top, text="Java", variable=radio, value=3, command=selection)
+R3.pack(anchor=W)
+
+label = Label(top)
+label.pack()
 top.mainloop()
