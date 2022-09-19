40f47e4720e287d588898c8cb4c2cd7982656194
fiona518 <whe036@aucklanduni.ac.nz>
2022-09-19 12:37:07 +1200

    tkinter frame

--- exercise.py
+++ exercise.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
-import tkinter as tk
-from functools import partial
+from tkinter import *
 
-def call_result(label_result, n1, n2):
-    num1 = (n1.get())
-    num2 = (n2.get())
-    result  = float(num1) + float(num2)
-    label_result.config(text="Resullt = %d" % result)
+top = Tk()
+top.geometry('140x100')
+frame = Frame(top)
+frame.pack()
 
-root = tk.Tk()
-root.geometry('400x200+100+200')
+leftframe = Frame(top)
+leftframe.pack(side=LEFT)
 
-root.title('Calculator')
+rightframe = Frame(top)
+rightframe.pack(side=RIGHT)
 
-number1 = tk.StringVar()
-number2 = tk.StringVar()
+btn1 = Button(frame, text="Submit", fg='red', activebackground='red')
+btn1.pack(side=LEFT)
 
-labelNum1 = tk.Label(root, text="A").grid(row=1, column=0)
-labelNum2 = tk.Label(root, text="B").grid(row=2, column=0)
+btn2 = Button(frame, text="Remove", fg='brown', activebackground='brown')
+btn2.pack(side=RIGHT)
 
-labelResult  = tk.Label(root)
-labelResult.grid(row=7, column=2)
+btn3 = Button(rightframe, text="Add", fg='blue', activebackground='blue')
+btn3.pack(side=LEFT)
 
-entryNum1  = tk.Entry(root, textvariable=number1).grid(row=1,  column=2)
-entryNum1  = tk.Entry(root, textvariable=number2).grid(row=2,  column=2)
+btn4 = Button(leftframe, text="Modify", fg='black', activebackground='white')
+btn4.pack(side=RIGHT)
 
-call_result = partial(call_result, labelResult, number1, number2)
-buttonCal = tk.Button(root, text="Calculate", command=call_result).grid(row=3, column=0)
-
-root.mainloop()
+top.mainloop()
