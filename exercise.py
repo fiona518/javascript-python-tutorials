6d7f35baa9359f9e25de257506801bea249a37e6
fiona518 <whe036@aucklanduni.ac.nz>
2022-09-19 12:56:57 +1200

    tkinter menu (bugged?)

--- exercise.py
+++ exercise.py
@@ -1,18 +1,34 @@
-#!/usr/bin/python3
-from tkinter import *
+from tkinter import Toplevel, Button, Tk, Menu  
+  
+top = Tk()  
+menubar = Menu(top)  
+file = Menu(menubar, tearoff=0)  
+file.add_command(label="New")  
+file.add_command(label="Open")  
+file.add_command(label="Save")  
+file.add_command(label="Save as...")  
+file.add_command(label="Close")  
+  
+file.add_separator()  
+  
+file.add_command(label="Exit", command=top.quit)  
+  
+menubar.add_cascade(label="File", menu=file)  
+edit = Menu(menubar, tearoff=0)  
+edit.add_command(label="Undo")  
+  
+edit.add_separator()  
 
-top = Tk()
-top.geometry('200x250')
-
-menubutton = Menubutton(top, text="Language", relief=FLAT)
-menubutton.grid()
-
-menubutton.menu = Menu(menubutton)
-menubutton["menu"] = menubutton.menu
-
-menubutton.menu.add_checkbutton(label="Hindi", variable= IntVar())
-menubutton.menu.add_checkbutton(label="English", variable= IntVar())
-
-menubutton.pack()
-
-top.mainloop()
+edit.add_command(label="Cut")  
+edit.add_command(label="Copy")  
+edit.add_command(label="Paste")  
+edit.add_command(label="Delete")  
+edit.add_command(label="Select All")  
+  
+menubar.add_cascade(label="Edit", menu=edit)  
+help = Menu(menubar, tearoff=0)  
+help.add_command(label="About")  
+menubar.add_cascade(label="Help", menu=help)  
+  
+top.config(menu=menubar)  
+top.mainloop() 
